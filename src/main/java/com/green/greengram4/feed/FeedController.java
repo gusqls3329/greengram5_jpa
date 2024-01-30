a272aadc67479a610593d4bd46bb1e52de34e34a
김현빈 <gusqls81017@gmail.com>
2024-01-30 10:57:02 +0900

    2024-01-30

--- src/main/java/com/green/greengram4/feed/FeedController.java
+++ src/main/java/com/green/greengram4/feed/FeedController.java
@@ -8,6 +8,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
